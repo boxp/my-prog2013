be621f90bc9f13923d281eec33368d3bb3334f2f
akameco <akame.meco@gmail.com>
2013-04-20 19:24:46 +0900

    滅びるがよい-なぜかうまくいかない-

---
+++ 2009.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void){
+    char x[3];
+    char tmp;
+    int i,j;
+
+    for(i=0; i<3; i++){
+        scanf("%c", &x[i]);
+        printf("%c\n", x[i]);
+    }
+
+    for(i=0; i<3; i++){
+        for(j = i+1; j<3; j++){
+            if( ( x[i] - x[j] ) > 0){
+                tmp = x[i];
+                x[i]=x[j];
+                x[j]=tmp;
+            }
+        }
+    }
+    return 0;
+}
