9bd5d89acf6116b3b9d6bb9f75fa320d7d796c61
akameco <akame.meco@gmail.com>
2013-04-20 21:41:47 +0900

    2009まじでできない滅びろ

--- 2013.c
+++ 2013.c
@@ -3,4 +3,12 @@ int main(void){
     char c; 
     scanf("%c", &c);
    
+    if(c > 0x40 && c < 0x5B){
+        printf("Large\n");
+    }else if(c > 0x60 && 0x7B){
+        printf("Small\n");
+    }else{
+        printf("Not an alphabet\n");
+    }
+    return 0;
 }
---
+++ 2015.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define N  1000;
+int main(void){
+    int x,y;
+    scanf("%d%d", &x, &y);
+    if(x * y == N){
+        printf("HIT\n");
+    }else{
+        printf("NG\n");
+    }
+    return 0;
+}
